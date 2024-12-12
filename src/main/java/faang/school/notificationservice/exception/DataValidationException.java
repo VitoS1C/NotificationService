d9176afdbc471c3725675fa4c2b9a2addbb0d3e2
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-12-12 20:12:29 +0500

    Deleted DataValidationException.java

--- src/main/java/faang/school/notificationservice/exception/DataValidationException.java
+++
@@ -1,7 +0,0 @@
-package faang.school.notificationservice.exception;
-
-public class DataValidationException extends RuntimeException {
-    public DataValidationException(String message, Throwable exception) {
-        super(message, exception);
-    }
-}
